9d8d9c854f1e50af2a9f610b2e2d4da12fceee38
Ankita Sharma <ankita.sharma@xoriant.com>
2021-10-21 13:48:00 +0530

    Added Components. Code formatting in Armorblox.py

--- Packs/Armorblox/Integrations/Armorblox/Armorblox.py
+++ Packs/Armorblox/Integrations/Armorblox/Armorblox.py
@@ -1,5 +1,6 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
 import dateparser
 import requests
 import json
@@ -10,31 +11,29 @@ from datetime import datetime, timedelta
 requests.packages.urllib3.disable_warnings()
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-MAX_INCIDENTS_TO_FETCH = 50
+# MAX_INCIDENTS_TO_FETCH = 50
 
 TENANT_NAME = demisto.params().get('tenantName')
 INSECURE = demisto.params().get('insecure')
 PROXY = demisto.params().get('proxy')
 API_KEY = demisto.params().get('apikey')
+
 BASE_URL = f"https://{TENANT_NAME}.armorblox.io/api/v1beta1/organizations/{TENANT_NAME}/incidents"
 
 payload: Dict = {}
 headers = {
     'x-ab-authorization': API_KEY
 }
-# contains all the incident severity types
-# IncidentSeverity = {
-#     'LOW' : 1,
-#     'MEDIUM' : 2,
-#     'HIGH' : 3
-# }
 
 
 def get_incident_message_ids(incident_id):
+    """
+    Returns the message ids for all the events for the input incident.
+    """
     incident_details_url = "{}/{}".format(BASE_URL, incident_id)
     detail_response = requests.request("GET", incident_details_url, headers=headers, data=payload).json()
     message_ids = []
-    # loop through all the events of this incident
+    # loop through all the events of this incident and collect the message ids
     if 'events' in detail_response.keys():
         for event in detail_response['events']:
             message_ids.append(event['message_id'])
@@ -46,8 +45,9 @@ def get_incident_message_ids(incident_id):
 
 
 def get_incidents():
-    # incident_list_url = f"https://{opt_tenant_name}.armorblox.io/api/v1beta1/organizations/{opt_tenant_name}/incidents"
-
+    """
+    Hits the Armorblox API and returns the list of fetched incidents.
+    """
     response = requests.request("GET", BASE_URL + "?&orderBy=ASC", headers=headers, data=payload)
 
     r_json = response.json()
@@ -67,16 +67,7 @@ def get_incidents():
     return results
 
 
-def get_armorblox_incidents_command():
-    results = get_incidents()
-    for incident in results:
-        occured_time = incident['date']
-        demisto.results('The user : ' + incident['users'] + 'has reported an alert as of ' + occured_time)
-
-
 def fetch_incidents_command():
-
-    # demisto.debug("Incidents: "+str(len(results)))
 
     last_run = demisto.getLastRun()
     if last_run and 'start_time' in last_run.keys():
@@ -87,45 +78,49 @@ def fetch_incidents_command():
         start_time = dateparser.parse(start_time.strftime(DATE_FORMAT))
     start_time = start_time.timestamp()
     demisto.debug(str(start_time))
-    data = get_incidents()
+    incidents_data = get_incidents()
     last_time = start_time
-    inc = []
-    for res in data:
-        # demisto.debug(res)
-        res['name'] = "Armorblox"
-        dt = res['date']
+    incidents = []
+    for incident in incidents_data:
+        dt = incident['date']
         dt = dateparser.parse(dt).timestamp()
+        # Update last run and add incident if the incident is newer than last fetch
         if dt > start_time:
 
-            # , 'details': json.dumps(res), 'severity': IncidentSeverity[res.get('priority')], 'CustomFields': {'details' : res}}
-            temp = {'name': "Armorblox", 'rawJSON': json.dumps(res), 'details': json.dumps(res), 'CustomFields': {'details': res}}
+            curr_incident = {'rawJSON': json.dumps(incident), 'details': json.dumps(incident)}
             last_time = dt
-            inc.append(temp)
+            incidents.append(curr_incident)
 
-        demisto.debug(str(last_time))
+    # Save the next_run as a dict with the start_time key to be stored
     demisto.setLastRun({'start_time': str(last_time)})
-    demisto.incidents(inc)
-    readable_output = f'## {inc}'
+    demisto.incidents(incidents)
+    readable_output = f'## {incidents}'
     return CommandResults(
         readable_output=readable_output,
         outputs_prefix='Armorblox',
         outputs_key_field='',
-        outputs=inc
+        outputs=incidents
     )
 
 
-''' EXECUTION '''
-LOG('command is %s' % (demisto.command(), ))
-try:
-    if demisto.command() == 'armorblox':
-        get_armorblox_incidents_command()
-    elif demisto.command() == "fetch-incidents":
-        a = fetch_incidents_command()
-        return_results(a)
-        demisto.results("Incidents fetched")
-    elif demisto.command() == 'test-module':
-        fetch_incidents_command()
-        demisto.results("OK")
-except Exception as e:
-    demisto.debug('Error!')
-    return_error(e)
+def test_module():
+    # Run a sample request to retrieve mock data
+    response = requests.request("GET", BASE_URL + "?&pageSize=1", headers=headers, data=payload)
+    demisto.results("ok")
+
+
+def main():
+    ''' EXECUTION '''
+    LOG('command is %s' % (demisto.command(), ))
+    try:
+        if demisto.command() == "fetch-incidents":
+            return_results(fetch_incidents_command())
+
+        elif demisto.command() == 'test-module':
+            test_module()
+    except Exception as e:
+        return_error(str(e))
+
+
+if __name__ in ['__main__', 'builtin', 'builtins']:
+    main()
